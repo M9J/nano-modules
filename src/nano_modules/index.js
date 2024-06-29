fed8d0400907d3e2359437bb694edd2a7a9b2458
Martian Jeirgif <mgj.exe@gmail.com>
2024-06-30 00:02:02 +0530

    nano_modules init commit

--- src/nano_modules/index.js
+++
@@ -1,8 +0,0 @@
-import Counter from "./Counter/Counter.js";
-import HelloWorld from "./HelloWorld/HelloWorld.js";
-import TodayDate from "./TodayDate/TodayDate.js";
-
-const INDEX = [TodayDate, HelloWorld, Counter];
-
-export default INDEX;
-// export default [];
