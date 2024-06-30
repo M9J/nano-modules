9d82eede31738b57ce616ae5854d220717cc3065
M9J <mnsh.exe@gmail.com>
2024-06-30 12:38:02 +0000

    Deploying to build from @ M9J/nano-modules@e36617d8a38d8ee84b9a8f3c2f1412726f0447ba 🚀

--- nano_modules/HelloWorld/HelloWorld.js
+++ nano_modules/HelloWorld/HelloWorld.js
@@ -1,1 +1,1 @@
-export default class HelloWorld{MODULE_NAME="Demo First Program";MODULE_DESCRIPTION="First Custom Nano Module";MODULE_VERSION="1.0";MODULE_MAIN=()=>"Hello world"}
+export default class HelloWorld{MODULE_NAME="Hello World";MODULE_DESCRIPTION="Hello World Nano Module";MODULE_VERSION="1.0";MODULE_MAIN=()=>"Hello world"}
