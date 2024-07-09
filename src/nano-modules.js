49d0bc6f63d21aac9e93408e85b1786e34fa9065
Martian Jeirgif <mgj.exe@gmail.com>
2024-07-09 18:04:16 +0530

    Minor fixes

--- src/nano-modules.js
+++ src/nano-modules.js
@@ -2,5 +2,6 @@ import NanoModules from "./app/NanoModules";
 import AppContainer from "./Components/AppContainer";
 import "./nano-modules.css";
 
-document.body.appendChild(new AppContainer());
+const app = new AppContainer();
+document.body.appendChild(app);
 NanoModules();
